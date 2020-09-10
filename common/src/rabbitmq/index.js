8ecbdacb4525b9e02f05e532785cb241283e1774
Binh Le <vanbinh87@gmail.com>
2020-09-10 22:59:23 +0700

    add func utilities of rabbitmq

---
+++ common/src/rabbitmq/index.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+const amqp = require('amqplib');
+
+
+
+
+async function create_channel(exchange_name, url = process.env.AMQP_URL) {
+
+    return amqp.connect(url).then(conn => {
+
+        return conn.createChannel();
+    }).then(async channel => {
+
+        let result_channel = await channel.assertExchange(exchange_name, 'topic', {
+            durable: false
+        });
+
+        return channel;
+    }).catch(error => {
+        
+        console.log(error);
+    })
+}
+
+async function send_message(exchange_name, message, routing_key, channel) {
+
+    return channel.publish(exchange_name, routing_key, Buffer.from(message));
+}
+
+async function receive_message(exchange_name, routing_key, queue_name, channel, callback) {
+
+    return channel.assertQueue(queue_name, {durable: true}, async function (error, ok) {
+
+        if (!error !== null) {
+            console.log('Error assert queue');
+            console.log(error);
+        }  
+    }).then(async ok => {
+        await channel.bindQueue(ok.queue, exchange_name, routing_key);
+        return ok.queue;  
+    }).then(queue => {
+
+        return channel.consume(queue, callback, { noAck: true });
+    });
+}
+
+
+(async () => {
+
+    const routing_key = 'order.created';
+    const exchange_name = 'purchase_channel';
+    const payment_queue_name = 'payment_queue';
+
+    let channel = await create_channel(exchange_name);
+    
+    await send_message(exchange_name, 'purchase for order id is ABC', routing_key, channel);
+
+    await receive_message(exchange_name, routing_key, payment_queue_name, channel, (msg) => {
+        console.log(`Received message with content: ${msg.content.toString()}`);
+    })
+
+    console.log('Done');
+})();
+
